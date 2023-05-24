ba3009f68cd509052e7aa5086a91c2368f3443cb
troylion56 <bluphoenix56@gmail.com>
2023-05-24 23:06:39 +0200

    aggioranemnti

--- src/main/java/it/uniroma3/siw/controller/RecensioniController.java
+++ src/main/java/it/uniroma3/siw/controller/RecensioniController.java
@@ -4,25 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import it.uniroma3.siw.model.Recensione;
-import it.uniroma3.siw.repository.RecensioneRepository;
+import it.uniroma3.siw.repository.MovieRepository;
+import it.uniroma3.siw.service.RecensioniService;
 
 @Controller
 public class RecensioniController {
     
 
     @Autowired
-    private RecensioneRepository recensioneRepository;
+    private RecensioniService recensioneService;
 
-    @GetMapping(value="/recensioni/formNewRecensione")
-    public String formNewRecensione(Model model){
-        model.addAttribute("recensione", new Recensione());
-        return "formNewRecensione.html";
-    }
+    @Autowired
+    private MovieRepository movieRepository;
 
     @GetMapping("/tutteLeRecensioni")
     public String paginaTutteRecensioni(Model model){
         System.out.println("RECENSIONI");
         return "/recensioni/tutteLeRecensioni.html";
     }
+
+    @GetMapping("/formNewRecensione")
+    public String formNewRecensione(Model model){
+        model.addAttribute("recensione", new Recensione());
+        model.addAttribute("movies", this.movieRepository.findAll());
+        return "/recensioni/formNewRecensione.html";
+    }
+
+    @PostMapping("/aggiungiRecensione")
+    public String aggiungiRecensione(@ModelAttribute("recensione") Recensione recensione) {
+        recensioneService.salvaRecensione(recensione);
+        return "test.html";
+    }
+
 }
--- src/main/java/it/uniroma3/siw/service/RecensioniService.java
+++ src/main/java/it/uniroma3/siw/service/RecensioniService.java
@@ -3,6 +3,7 @@ package it.uniroma3.siw.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.uniroma3.siw.model.Recensione;
 import it.uniroma3.siw.repository.RecensioneRepository;
 
 @Service
@@ -10,5 +11,8 @@ public class RecensioniService {
 
     @Autowired
     private RecensioneRepository recensioneRepository;
-     
+
+    public void salvaRecensione(Recensione recensione) {
+        recensioneRepository.save(recensione);
+    }
 }
