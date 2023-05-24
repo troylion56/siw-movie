fcf1933f11cbdc677427923bb9b973068e28d871
troylion56 <bluphoenix56@gmail.com>
2023-05-24 18:09:41 +0200

    aggioranemnti

--- src/main/java/it/uniroma3/siw/controller/RecensioniController.java
+++ src/main/java/it/uniroma3/siw/controller/RecensioniController.java
@@ -3,6 +3,7 @@ package it.uniroma3.siw.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,6 +12,7 @@ import it.uniroma3.siw.model.Recensione;
 import it.uniroma3.siw.repository.MovieRepository;
 import it.uniroma3.siw.repository.RecensioneRepository;
 
+@Controller
 public class RecensioniController {
     
 
@@ -23,8 +25,9 @@ public class RecensioniController {
         return "formNewRecensione.html";
     }
 
-    @GetMapping("/recensioni/tutteLeRecensioni")
+    @GetMapping("/tutteLeRecensioni")
     public String paginaTutteRecensioni(Model model){
-        return "/recensioni/tutteLeRecensioni.html";
+        System.out.println("RECENSIONI");
+        return "tutteLeRecensioni.html";
     }
 }
